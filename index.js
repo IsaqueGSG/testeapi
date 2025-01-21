eb95cd86b1608101bb464ab3936e1912d49f14ed
IsaqueGSG <isaquegsgoncalves@gmail.com>
2025-01-21 11:17:27 -0300

    testado 3

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const express = require('express');
 const app = express();
 const client = new Client({
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-zygote',
+          '--single-process',
+          '--disable-gpu',
+        ],
+        headless: true,
     },
+    
 });
 
 client.on('qr', (qr) => {
